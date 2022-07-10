dd47f04b5f180c6f5bfb1817f3360958875114ef
Viktor Holovin <holovin@mail.com>
2022-07-10 16:38:42 +0200

    Data types

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello, world!");
+    byte smallNumber = 127;
+    byte smallNegativeNumber = -128;
+    short shortNumber = 32767;
+    short shortNegativeNumber = -32768;
+    int number = 2147483647;
+    int negativeNumber = -2147483648;
+    System.out.println(smallNumber);
   }
 }
