f64c285e16e2d17adb724273a34c75737f700565
Viktor Holovin <holovin@mail.com>
2022-07-10 18:05:02 +0200

    Switch test

--- Main.java
+++ Main.java
@@ -7,6 +7,20 @@ public class Main {
     short shortNegativeNumber = -32768;
     int number = 2147483647;
     int negativeNumber = -2147483648;
-    System.out.println(smallNumber);
+    long longNumber = 9223372036854775807L;
+    long longNegativeNumber = -9223372036854775808L;
+    float floatNumber = 5.1234567f;
+    double doubleNumber = 19.123456789d;
+    System.out.println(floatNumber);
+    boolean isJavaFun = true;
+    boolean isFishTasty = false;
+    System.out.println(isJavaFun);     // Outputs true
+    System.out.println(isFishTasty);   // Outputs false
+    char myGrade = 'B';
+    System.out.println(myGrade);
+    String txt = "Hello, world!";
+    System.out.println(txt.toUpperCase());   // Outputs "HELLO WORLD"
+    System.out.println(txt.toLowerCase());
+    System.out.println(Math.random());
   }
 }
