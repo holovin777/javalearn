113f68da1e22fa564365293895c140da7f688927
Viktor Holovin <holovin@mail.com>
2022-07-11 11:40:19 +0200

    Import package

--- Br.java
+++ Br.java
@@ -1,4 +1,10 @@
 public class Br {
+  static void myMethod(String fname, int age) {
+    System.out.println(fname + " Refsnes" + " is " + age);
+  }
+  static int returnMethod(int x, int y) {
+    return x + y;
+  }
   public static void main(String[] args) {
     for (int i = 0; i < 10; i++) {
       if (i == 4) {
@@ -6,5 +12,8 @@ public class Br {
       }
       System.out.println(i);
     }
+    myMethod("Liam", 20);
+    myMethod("Jenny", 45);
+    System.out.println(returnMethod(3, 5));
   }
 }
---
+++ CreatePerson.java
@@ -0,0 +1,7 @@
+public class CreatePerson {
+  public static void main(String[] args) {
+    Person myPerson = new Person();
+    myPerson.setName("John");
+    System.out.println(myPerson.getName());
+  }
+}
